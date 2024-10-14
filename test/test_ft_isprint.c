456199d3a91d468c3c9d8298638ae6b27d6870ed
Marcio Daniel Herminio <mhermini@c2r8p3.42sp.org.br>
2024-10-14 20:29:09 -0300

    test: add test_ft_isprint

---
+++ test/test_ft_isprint.c
@@ -0,0 +1,38 @@
+#include "../src/libft.h"
+#include "../unity/unity.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_ft_isprint_valid_chars(void)
+{
+    char start = 32;
+    while (start <= 126)
+    {
+        TEST_ASSERT_TRUE(ft_isprint(start), "Expected valid printable character");
+        start += 1;
+    }
+}
+
+void test_ft_isprint_invalid_chars(void)
+{
+    char start = 1;
+    while (start <= 31)
+    {
+        TEST_ASSERT_FALSE(ft_isprint(start), "Expected invalid non-printable character");
+        start += 1;
+    }
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_ft_isprint_valid_chars);
+    RUN_TEST(test_ft_isprint_invalid_chars);
+    return UNITY_END();
+}
