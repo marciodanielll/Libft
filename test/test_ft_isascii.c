8cc1fbac0acd4e041b984d74c03cf9e7343e1457
Marcio Daniel Herminio <mhermini@c2r8p3.42sp.org.br>
2024-10-14 20:20:00 -0300

    test: add test_is_ascii

---
+++ test/test_ft_isascii.c
@@ -0,0 +1,33 @@
+#include "../src/libft.h"
+#include "../unity/unity.h"
+
+void	setUp(void)
+{
+}
+void	tearDown(void)
+{
+}
+
+void	test_ft_isascii_valid_digits(void)
+{
+    int start = 1;
+    while(start <=126)
+    {
+        TEST_ASSERT_TRUE(ft_isascii(start));
+        start++;
+    }
+}
+
+void	test_ft_isascii_valid_invalids(void)
+{
+    TEST_ASSERT_FALSE(ft_isascii(-10));
+    TEST_ASSERT_FALSE(ft_isascii(-127));
+}
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_ft_isascii_valid_digits);
+	RUN_TEST(test_ft_isascii_valid_invalids);
+	return (UNITY_END());
+}
