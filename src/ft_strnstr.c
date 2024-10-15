3ed4ff5053aaade7fdac7a98c26e96d5c5a30faf
Marcio Daniel Herminio <mhermini@c3r10p5.42sp.org.br>
2024-10-15 14:08:45 -0300

    feat:add ft_strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -6,16 +6,34 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:56:19 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/15 12:31:55 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/15 14:08:01 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stddef.h>
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	(void)big;
-	(void)little;
-	(void)len;
-	return (char *)(big);
+	size_t	i;
+	size_t	j;
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (i < len && haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (i + j < len && needle[j] != '\0'
+				&& haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+			if (needle[j] == '\0')
+				return ((char *)&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
