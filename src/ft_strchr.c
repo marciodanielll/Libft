e1d2359bdafab0d845d4c8ef88f9b9aba612acbf
Marcio Daniel Herminio <mhermini@c1r6p2.42sp.org.br>
2024-10-15 10:35:38 -0300

    feat: add ft_strchr

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,17 +6,29 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 09:39:56 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/15 09:54:10 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/15 10:35:18 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
-	char	*ptr;
-	char	str;
-
-	(void)s;
-	(void)c;
-	ptr = &str;
-	return (ptr);
+	if (s == NULL)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	while (*s)
+	{
+		if (*s == (const char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
--- test/test_ft_strchr.c
+++ test/test_ft_strchr.c
@@ -11,11 +11,12 @@ void	tearDown(void)
 void	test_ft_strchr_success(void)
 {
 	const char	*testString = "Marcio Daniel";
-	size_t		length = sizeof(testString) - 1;
+	size_t		length;
 	size_t		i;
-	char		*charPointers[length];
+	char		*charPointers[sizeof(testString) - 1];
 	char		*result;
 
+	length = sizeof(testString) - 1;
 	i = 0;
 	while (i < length)
 	{
@@ -26,26 +27,42 @@ void	test_ft_strchr_success(void)
 	while (i < length)
 	{
 		result = ft_strchr(testString, *charPointers[i]);
-	
-			TEST_ASSERT_EQUAL(*charPointers[i], *result);
-
+		TEST_ASSERT_EQUAL(*charPointers[i], *result);
 		i++;
 	}
 }
 
-void test_ft_strchr_error(void)
+void	test_ft_strchr_error(void)
 {
-    const char *testString = "Marcio Daniel";
-    char charsToTest[] = {'x', 'y', 'z', '!', '@', '1', ' '};
-    size_t i = 0;
-    char *result;
+	const char	*testString = "Marcio Daniel";
+	char		charsToTest[] = {'x', 'y', 'z', '!', '@', '1'};
+	size_t		i;
+	char		*result;
 
-    while (i < sizeof(charsToTest))
-    {
-        result = ft_strchr(testString, charsToTest[i]);
-        TEST_ASSERT_NULL(result);
-        i++;
-    }
+	i = 0;
+	while (i < sizeof(charsToTest))
+	{
+		result = ft_strchr(testString, charsToTest[i]);
+		TEST_ASSERT_NULL(result);
+		i++;
+	}
+}
+
+void	test_ft_strchr_null_pointer(void)
+{
+	char	*result;
+
+	result = ft_strchr(NULL, 'x');
+	TEST_ASSERT_NULL(result);
+}
+
+void	test_ft_strchr_null_character(void)
+{
+	const char	*testString = "Marcio Daniel";
+	char		*result;
+
+	result = ft_strchr(testString, '\0');
+	TEST_ASSERT_EQUAL_PTR(result, testString + sizeof("Marcio Daniel") - 1);
 }
 
 int	main(void)
@@ -53,5 +70,7 @@ int	main(void)
 	UNITY_BEGIN();
 	RUN_TEST(test_ft_strchr_success);
 	RUN_TEST(test_ft_strchr_error);
+	RUN_TEST(test_ft_strchr_null_pointer);
+	RUN_TEST(test_ft_strchr_null_character);
 	return (UNITY_END());
 }
