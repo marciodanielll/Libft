c525f81df35cac8e2849c67e5fbae6d573e64d0d
Márcio Daniel <marcio.daniel@msn.com>
2024-10-21 18:56:00 +0000

    feat: add ft_stren

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 15:29:12 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/21 15:53:36 by mhermini         ###   ########.fr       */
+/*   Created: 2024/10/21 15:54:47 by mhermini          #+#    #+#             */
+/*   Updated: 2024/10/21 15:54:52 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
 
-size_t	ft_strlen(const char *s);
-
-#endif /* LIBFT_H */
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
