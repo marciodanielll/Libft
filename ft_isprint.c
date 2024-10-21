5688aba845439f8a59aaa51d48ce2433caf4ad80
Márcio Daniel <marcio.daniel@msn.com>
2024-10-21 18:49:11 +0000

    feat: add f_isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 15:29:12 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/21 15:42:33 by mhermini         ###   ########.fr       */
+/*   Created: 2024/10/21 15:42:53 by mhermini          #+#    #+#             */
+/*   Updated: 2024/10/21 15:44:20 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif /* LIBFT_H */
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
