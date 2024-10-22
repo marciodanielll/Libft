e7fdfd754e47a3a7f88b2736e4d0232f5f5491c3
Márcio Daniel <marcio.daniel@msn.com>
2024-10-22 00:58:07 +0000

    feat: ft_toupper ft_tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:29:12 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/21 21:42:20 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/21 21:57:12 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif /* LIBFT_H */
