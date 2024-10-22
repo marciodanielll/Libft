e2d4cf19ff9003093a790c3ad5af12f4820a48b6
Márcio Daniel <marcio.daniel@msn.com>
2024-10-22 00:52:11 +0000

    feat: add ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:29:12 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/21 21:06:09 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/21 21:42:20 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif /* LIBFT_H */
