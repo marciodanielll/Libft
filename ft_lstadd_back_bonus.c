3806bdb6572aefa7e274ca3abbadbfb287a95f78
Márcio Daniel <marcio.daniel@msn.com>
2024-10-28 00:06:17 +0000

    refactor: improve lstadd_back lstadd_front

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:26:36 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/27 18:36:20 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/27 20:59:39 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (*lst == NULL)
 	{
 		ft_lstadd_front(lst, new);
-		return ;
 	}
-	last = ft_lstlast(*lst);
-	last->next = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
 }
