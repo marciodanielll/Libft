dd5ab271d6690ce3f137ac9d2902e1397900b35a
Márcio Daniel <marcio.daniel@msn.com>
2024-10-28 21:00:19 -0300

    fix: solve use del

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 19:11:03 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/27 19:24:30 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/28 20:51:46 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*ptr;
 	t_list	*new_elem;
+	void	*content;
 
 	ptr = NULL;
-	if (lst != NULL && f != NULL)
+	if (!lst || !f)
+		return (NULL);
+	while (lst)
 	{
-		while (lst)
+		content = f(lst->content);
+		new_elem = ft_lstnew(content);
+		if (!new_elem)
 		{
-			new_elem = ft_lstnew(f(lst->content));
-			if (!new_elem)
-			{
-				ft_lstclear(&ptr, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&ptr, new_elem);
-			lst = lst->next;
+			if (del)
+				del(content);
+			ft_lstclear(&ptr, del);
+			return (NULL);
 		}
+		ft_lstadd_back(&ptr, new_elem);
+		lst = lst->next;
 	}
 	return (ptr);
 }
