fd990d4a886db787bc8347c979172a3b8b934205
Márcio Daniel <marcio.daniel@msn.com>
2024-10-30 18:45:19 -0300

    style: adjust linter ft_calloc~

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 22:03:35 by mhermini          #+#    #+#             */
-/*   Updated: 2024/10/30 18:38:11 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/10/30 18:42:14 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	{
 		ptr = (void *)malloc(0);
 		if (!ptr)
-		{
 			return (NULL);
-		}
 		return (ptr);
-	};
+	}
 	if (nmemb && (size > (((size_t)-1) / nmemb)))
 		return (NULL);
 	total_size = nmemb * size;
